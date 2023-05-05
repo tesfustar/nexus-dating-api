32a561b6c554cb2acbe2f9d36cf8ffe4974f6171
Tesfaye Lemi <tesfu9503@gmail.com>
2023-05-05 05:56:04 -0700

    check for deployment and mongo deployment

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 mongoose.set("strictQuery", true)
 const mongoDbConnection = mongoose
-  .connect('mongodb://0.0.0.0:27017/grocery')
+  .connect('mongodb://31.220.18.115:27017/my-database')
   .then(() => console.log("db connected"))
   .catch((err) => console.log(`error ${err}`));
 
